8e1ebe865d8b7640de95e526933c231a570fea29
weslleycz <weslleysouza22@gmail.com>
2024-03-19 19:44:26 -0300

    Resolvendo o desafio da aula 6

--- aula6/src/main/java/com/aula6/controllers/mathController/MathController.java
+++ aula6/src/main/java/com/aula6/controllers/mathController/MathController.java
@@ -1,15 +1,23 @@
-package com.aula6;
+package com.aula6.controllers.mathController;
 
+import com.aula6.services.SimpleMathService;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
+@Validated
 class MathController {
     private final AtomicLong counter = new AtomicLong();
+    private final SimpleMathService mathService;
+
+    MathController(SimpleMathService mathService) {
+        this.mathService = mathService;
+    }
 
     @GetMapping(value = "/sum/{numberOne}/{numberTwo}")
     public Object sun(
@@ -20,10 +28,59 @@ class MathController {
             @Pattern(regexp = "^\\d+$", message = "O número deve conter apenas dígitos") String numberTwo
     ) {
         try {
-            Double result = Double.parseDouble(numberOne) + Double.parseDouble(numberTwo);
-            return ResponseEntity.ok(result).getBody();
+            return this.mathService.sum( Double.parseDouble(numberOne), Double.parseDouble(numberTwo));
+        } catch (NumberFormatException e) {
+            return ResponseEntity.badRequest().body("Erro ao converter números");
+        }
+    }
+
+    @GetMapping(value = "/subtraction/{numberOne}/{numberTwo}")
+    public Object subtraction(
+            @PathVariable(value = "numberOne") @NotBlank
+            @Pattern(regexp = "^\\d+$", message = "O número deve conter apenas dígitos") String numberOne,
+
+            @PathVariable(value = "numberTwo") @NotBlank
+            @Pattern(regexp = "^\\d+$", message = "O número deve conter apenas dígitos") String numberTwo
+    ) {
+        try {
+            return this.mathService.subtraction( Double.parseDouble(numberOne), Double.parseDouble(numberTwo));
+        } catch (NumberFormatException e) {
+            return ResponseEntity.badRequest().body("Erro ao converter números");
+        }
+    }
+
+    @GetMapping(value = "/multiplication/{numberOne}/{numberTwo}")
+    public Object multiplication(
+            @PathVariable(value = "numberOne") @NotBlank
+            @Pattern(regexp = "^\\d+$", message = "O número deve conter apenas dígitos") String numberOne,
+
+            @PathVariable(value = "numberTwo") @NotBlank
+            @Pattern(regexp = "^\\d+$", message = "O número deve conter apenas dígitos") String numberTwo
+    ) {
+        try {
+            return this.mathService.multiplication( Double.parseDouble(numberOne), Double.parseDouble(numberTwo));
+        } catch (NumberFormatException e) {
+            return ResponseEntity.badRequest().body("Erro ao converter números");
+        }
+    }
+
+    @GetMapping(value = "/division/{numberOne}/{numberTwo}")
+    public Object division(
+            @PathVariable(value = "numberOne") @NotBlank
+            @Pattern(regexp = "^\\d+$", message = "O número deve conter apenas dígitos") String numberOne,
+
+            @PathVariable(value = "numberTwo") @NotBlank
+            @Pattern(regexp = "^\\d+$", message = "O número deve conter apenas dígitos") String numberTwo
+    ) {
+        try {
+            return this.mathService.division( Double.parseDouble(numberOne), Double.parseDouble(numberTwo));
         } catch (NumberFormatException e) {
             return ResponseEntity.badRequest().body("Erro ao converter números");
         }
     }
 }
+
+
+
+
+
