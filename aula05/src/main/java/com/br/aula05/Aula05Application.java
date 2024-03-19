8daf7b1b7c0146b133d508aafe04cf9451abe2b0
weslleycz <weslleysouza22@gmail.com>
2024-03-19 01:04:30 -0300

    aula 05

---
+++ aula05/src/main/java/com/br/aula05/Aula05Application.java
@@ -0,0 +1,12 @@
+package com.br.aula05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula05Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Aula05Application.class, args);
+	}
+
+}
