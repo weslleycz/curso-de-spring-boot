94f82f1aec5df0a4e935d22cb4bd38872b04f8c5
weslleycz <weslleysouza22@gmail.com>
2024-03-20 23:32:10 -0300

    fix

--- aula8/src/main/java/com/aula8/Aula8Application.java
+++ aula8/src/main/java/com/aula8/Aula8Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Aula8Application {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(Aula8Application.class, args);
 	}
 
