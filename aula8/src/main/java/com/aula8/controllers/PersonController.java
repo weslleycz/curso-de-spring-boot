e8020aec45ea02fa6a1dfb5bc37c176de4b3d88a
weslleycz <weslleysouza22@gmail.com>
2024-05-03 21:59:42 -0300

    Adicionando Suporte à Migrations com Flyway

--- aula8/src/main/java/com/aula8/controllers/PersonController.java
+++ aula8/src/main/java/com/aula8/controllers/PersonController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/api/person/v1")
 public class PersonController {
 
     @Autowired
